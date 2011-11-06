dbfc11971b45539805073ad98c387f1ac6d99e1f
Jaisen Mathai <jaisen@jmathai.com>
2011-11-06 13:05:08 -0800

    Fixing typo.

--- templates/contact.php
+++ templates/contact.php
@@ -1,5 +1,4 @@
 <h1>We'd love to hear from you</h1>
 <p>
-We would love to hear from your. 
 If you have questions or would simply like to learn more you can send an email to Mathew directly at <a href="mailto:mattmathai@hotmail.com">mattmathai@hotmail.com</a>.
 </p>
