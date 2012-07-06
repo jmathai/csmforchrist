0cfe3c0e212a460312c4b84bc4d3c3046d758e45
Jaisen Mathai <jaisen@jmathai.com>
2012-07-06 11:11:36 -0700

    Updating bikes to Sidlaghatta

--- templates/updates/1.1-Sidlaghatta-june-2012.php
+++ templates/updates/1.1-Sidlaghatta-june-2012.php
@@ -1,12 +1,12 @@
 <?php
   $size = '150x150xCR';
-  $ch = curl_init("http://csmforchrist.openphoto.me/photos/list.json?pageSize=0&tags=csmbikes&returnSizes={$size},800x800");
+  $ch = curl_init("http://csmforchrist.openphoto.me/photos/list.json?pageSize=0&tags=Sidlaghatta,June,2012&returnSizes={$size},800x800");
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   $response = json_decode(curl_exec($ch), 1);
   curl_close($ch);
   $photos = $response['result'];
 ?>
-<h1>Photos from VBS, 2012</h1>
+<h1>Sidlaghatta Construction Update, June 2012</h1>
 <p>
   <?php foreach($photos as $photo) { ?>
     <a href="<?php echo $photo["path800x800"]; ?>" class="lightbox"><img src="<?php echo $photo["path{$size}"]; ?>"></a>
